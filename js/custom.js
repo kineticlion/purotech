42647f39cda6301deabe21bf8ca2fd1c46b78791
Sufiyan Saboowala <sufiyansaboowala13@gmail.com>
2020-11-06 13:07:14 -0500

    Fixed footer styles

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-//SMoothscroll
+//Smoothscroll
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -84,7 +84,59 @@ navLinks.forEach(link =>
 );
 
 //FORM
+
+const validateInputs = () => {
+  const firstName = document.getElementById('firstName');
+  const firstNameValue = firstName.value.trim();
+  const lastName = document.getElementById('lastName');
+  const lastNameValue = lastName.value.trim();
+  const phone = document.getElementById('email');
+  const email = document.getElementById('phone');
+  const emailValue = email.value.trim();
+  const phoneValue = phone.value.trim();
+  
+  if (!firstNameValue || firstNameValue === '') {
+    if (!firstName.classList.contains('error')) {
+      firstName.classList.add('error');
+      }
+  } else {
+    firstName.classList.remove('error')
+  }
+
+  if (!lastNameValue || lastNameValue === '') {
+    if (!lastName.classList.contains('error')) {
+      lastName.classList.add('error');
+      }
+  } else {
+    lastName.classList.remove('error')
+  }
+
+
+
+
+  const emailPatterm = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum|in|io)\b/;
+  if (!emailValue.match(emailPatterm)) {
+    if (!email.classList.contains('error')) {
+      email.classList.add('error');
+    } else {
+      email.classList.remove('error');
+      }
+  }
+
+  const phonePattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  if (!phoneValue.match(phonePattern)) {
+    phone.classList.add('error');
+  } else {
+    phone.classList.remove('error');
+  }
+}
+
+
 document.getElementById("mainForm").addEventListener("submit", e => {
- e.preventDefault();
+  e.preventDefault();
+  
+  validateInputs();
  console.log("Form submitted");
 });
+
+
