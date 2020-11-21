c5c1e7507b31085b9713c9867d231f161d640045
Sufiyan Saboowala <sufiyansaboowala13@gmail.com>
2020-11-21 14:50:54 -0500

    Updated form

--- js/custom.js
+++ js/custom.js
@@ -84,8 +84,6 @@ navLinks.forEach(link =>
 );
 
 //FORM
-
-
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
@@ -135,5 +133,37 @@ document.querySelector('#mainForm').addEventListener('submit', (e) => {
 })
 
 
+// #sendMailForm takes the data from the form with given ID
+$('#mainForm').submit(function (e) {
+  console.log("ji")
+  var data = {
+      'name': $('#firstName').val(),
+      'email': $('#email').val(),
+      'contact': $('#contactNumber').val(),
+      'message' : $('#message').val()
+  };
+  // POST data to the php file
+  $.ajax({ 
+      url: '../mail.php', 
+      data: data,
+      type: 'POST',
+      success: function (data) {
+    // For Notification
+          document.getElementById("sendMailForm").reset();
+          var $alertDiv = $(".mailResponse");
+          $alertDiv.show();
+          $alertDiv.find('.alert').removeClass('alert-danger alert-success');
+          $alertDiv.find('.mailResponseText').text("");
+          if(data.error){
+              $alertDiv.find('.alert').addClass('alert-danger');
+              $alertDiv.find('.mailResponseText').text(data.message);
+          }else{
+              $alertDiv.find('.alert').addClass('alert-success');
+              $alertDiv.find('.mailResponseText').text(data.message);
+          }
+      }
+  });
+  e.preventDefault();
+});
 
 
